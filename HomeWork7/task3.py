2402d69607163724cc046d6d28ba01c1f08bbccc
DESKTOP-DFUMFGM\Arlan <arlan.tleuberlin@murka.com>
2023-09-18 19:19:51 +0300

    r

--- HomeWork7/task3.py
+++ HomeWork7/task3.py
@@ -2,14 +2,18 @@ def display_box(width: int, height: int, character="*"):
     print(width * character)
     for i in range(height):
         if i % 2 == 0:
-            print(character + ' '*(width-2) + character)
+            print(character + ' ' * (width - 2) + character)
     print(width * character)
 
 
-for_user = input("Введіть довжину, висоту та символ: ")
+for_user = input("Введіть довжину, висоту та за бажанням символ: ")
 elements = for_user.split()
-width = int(elements[0])
-height = int(elements[1])
-char = str(elements[2])
+width_1 = int(elements[0])
+height_1 = int(elements[1])
+len_inp = for_user.split()
 if __name__ == '__main__':
-    display_box(width, height, char)
+    if len(len_inp) == 3:
+        character_1 = elements[2]
+        display_box(width_1, height_1, character_1)
+    else:
+        display_box(width_1, height_1)
